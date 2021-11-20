179abae9bda57bd805312b5931485816fed7dc69
Ryan OTX <84174550+RotX18@users.noreply.github.com>
2021-11-21 00:00:18 +0800

    Revert "updates + added movement from asset store" (#6)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
+    //KEN SEE IF YOU CAN DO THE UI BUTTONDOWN/ JOYSTICK IF NOT WE JUST USE MOVE POS
+
     //CONTROLS THE PLAYER MOVEMENT VIA RIGIDBODY
     //public vars
     public static PlayerController instance;
+    public Button btnLeft;
+    public Button btnRight;
+    public Button btnJump;
     [HideInInspector] public float moveDist = 0.5f;
     [HideInInspector] public float jumpStrength = 1f;
     [HideInInspector] public float speed = 5f;
@@ -35,6 +40,10 @@ public class PlayerController : MonoBehaviour
         else if(instance != this) {
             Destroy(gameObject);
         }
+
+        btnLeft.onClick.AddListener(left);
+        btnRight.onClick.AddListener(right);
+        btnJump.onClick.AddListener(jump);
     }
 
     // Start is called before the first frame update
@@ -42,20 +51,29 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
     }
 
-    void FixedUpdate() { 
+    /*void FixedUpdate() {
+        //controllerValue works in place of Input.GetAxis();
         //horizontal axis
-        rb.AddForce(CrossPlatformInputManager.GetAxis("Horizontal") * speed * Vector2.right, ForceMode2D.Impulse);
+        rb.AddForce(Input.GetAxis("Horizontal") * speed * Vector2.right, ForceMode2D.Impulse);
         rb.velocity = Vector2.ClampMagnitude(rb.velocity, maxSpeed);
 
         //vertical, jumping
-        if(CrossPlatformInputManager.GetButtonDown("Jump")){
-            //CHECK FOR GROUNDED, TEST FIRST THEN ADD CHECK
-            rb.AddForce(Vector2.up * jumpStrength);
+        if(_grounded == true && Input.GetKeyDown(Keycode.Space)) { //NOT WORKING, FIX
+            _grounded = false;
+            rb.AddForce(Vector2.up * jumpStrength, ForceMode2D.Impulse);
         }
+    }*/
 
-        //stopping motion if nothing is pressed
-        if(CrossPlatformInputManager.GetAxis("Horizontal") == 0){
-            rb.velocity = new Vector2(0 , rb.velocity.y);
-        }
+    private void jump(){
+        rb.MovePosition(new Vector2(transform.position.x, transform.position.y + jumpStrength));
+    }
+
+    private void left(){
+        //moving the rb by 1 (value of moveDist) unit
+        rb.MovePosition(new Vector2(transform.position.x - moveDist, transform.position.y));
+    }
+
+    private void right(){
+        rb.MovePosition(new Vector2(transform.position.x + moveDist, transform.position.y));
     }
 }
