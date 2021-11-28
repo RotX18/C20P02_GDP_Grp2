d39fc1290613202b88bbbb97433a0b1634ec1e3a
kachik0 <55240635+kachik0@users.noreply.github.com>
2021-11-28 22:32:05 +0800

    Completed game UI/manager
    In-game timer complete
    part of the level done to showcase gameplay
    scoring and live ui is done left coding to do when i have enemy

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System.Diagnostics;
+using System.Threading;
+
 
 public class GameManager : MonoBehaviour
 {
+
+    public Text timerText;
+    private float seconds;
+    private int minute;
+    private int hour;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +23,17 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        TimerUI();
+    }
+    void TimerUI()
+    {
+        seconds += Time.deltaTime;
+        timerText.text =  minute.ToString("00") + ":" + seconds.ToString("00");
+        if (seconds >= 60)
+        {
+            minute++;
+            seconds = 0;
+        }
+
     }
 }
